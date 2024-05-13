9d3777be4e56e2bf9ffd1db03ee4d7dc7b486e9f
stephen dewyer <stephen.dewyer@stephengdewyer.info>
2024-05-12 20:22:58 -0500

    test

--- src/auth.js
+++ src/auth.js
@@ -2,7 +2,7 @@ import { SvelteKitAuth } from "@auth/sveltekit";
 import Credentials from "@auth/core/providers/credentials";
 import { AUTH_SECRET } from '$env/static/private';
 import { clientAuthentication } from "$lib/server/authentication/client-authentication.js";
-
+// test
 export const { handle, signIn, signOut } = SvelteKitAuth({
     providers: [
         Credentials({
